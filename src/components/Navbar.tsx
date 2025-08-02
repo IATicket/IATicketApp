ada376373c3de569bf029cfcf43923c7dcaa9d82
John <john.moreno@ibm.com>
2025-08-02 20:10:26 +0200

    fix:rename variables

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
   const handleLogout = async () => {
     await signOut({ redirect: false });
     if (session && session.idToken) {
-      const keycloakIssuer = process.env.NEXT_PUBLIC_KEYCLOAK_ISSUER;
+      const keycloakIssuer = process.env.KEYCLOAK_ISSUER;
       const callbackUrl = window.location.origin;
       const logoutUrl = `${keycloakIssuer}/protocol/openid-connect/logout?id_token_hint=${session.idToken}&post_logout_redirect_uri=${callbackUrl}`;
       router.push(logoutUrl);
